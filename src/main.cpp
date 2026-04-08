e7f3d7d65891d973fe48d4596d92c39e089b03cc
Ky Ngo <kyngo992@gmail.com>
2026-04-07 23:43:29 -0600

    MNIST digits dataset + start of IDX file reader

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,22 @@
+#include <fstream>
 #include <iostream>
-#include "../include/NeuralNetwork.h"
+#include <string>
+
 #include "../include/Mat2D.h"
+#include "../include/NeuralNetwork.h"
 
 int main() {
+
+    std::string file = "trunk/train-labels-idx1-ubyte"; 
+    std::ifstream fstream(file, std::ios::binary);
+
+    if (fstream.is_open() == false) {
+        std::cout << "File failed to open" << std::endl;
+    }
+    
     std::vector<size_t> layers = {2, 3, 3, 4, 5, 1};
     NeuralNetwork layeredNetwork(layers);
     
-    std::cout << layeredNetwork << std::endl;    
+//    std::cout << layeredNetwork << std::endl;    
     return 0;    
 }
